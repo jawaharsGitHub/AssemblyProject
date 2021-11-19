9c973fce33acb7dfb46b66f10295108fbf7618f8
jawaharsGitHub <jawahars@live.in>
2021-11-19 23:19:37 +0530

    ddl change event not working as expected!

---
+++ AssemblyApp/LearningApp/Literals.py
@@ -0,0 +1,42 @@
+# print('a\bc')
+# print('it\'s')
+# print('\101')
+# print('\x41 ')
+# print("\\")
+
+def Test():
+    """Example function with types documented in the docstring.
+
+        `PEP 484`_ type annotations are supported. If attribute, parameter, and
+        return types are annotated according to `PEP 484`_, they do not need to be
+        included in the docstring:
+
+        Args:
+            param1 (int): The first parameter.
+            param2 (str): The second parameter.
+
+        Returns:
+            bool: The return value. True for success, False otherwise.
+
+        .. _PEP 484:
+            https://www.python.org/dev/peps/pep-0484/
+
+        """
+
+    pass
+
+# print(Test.__doc__)
+# help(Test)
+# print(bytes(10))
+
+# print(hex(ord('!')))
+
+# print(chr(65))
+
+# x = bytes(((2,3)))
+# x= u"\N{SOCCER BALL} \N{BLACK SPADE SUIT}"
+
+# x = '\UFFFFFFFF'
+# print(x)
+
+
--- AssemblyApp/adangalApp/views.py
+++ AssemblyApp/adangalApp/views.py
@@ -1,24 +1,40 @@
 from django.shortcuts import render, HttpResponse
 from Common.Tamilnadu import TNapi
+
+
 # from static import tnDataSource
 
 # Create your views here.
 
 
 def adangal(request):
-    # if request.method == 'GET':
-        districts = TNapi.get_districts()
-        taluks = TNapi.get_taluk(17)
-        villages = TNapi.get_villages(17, '05')
-        print('districts')
+    districts = TNapi.get_districts()
+    context = None
+    if request.method == 'GET':
+        print('GET')
         context = {
-            'districts': districts,
-            'taluks': taluks,
-            'villages': villages
-
+            'districts': districts
         }
         return render(request, 'adangal/adangal_home.html', context)
-    # else:
-    #     return render(request, 'adangal/adangal_home.html')
+    elif request.method == 'POST':
+        print('POST')
 
+        try:
+            mid = request.POST['ddlMaavattam'];
+            taluks = TNapi.get_taluk(mid.zfill(2))
+            context = {
+                'districts': districts,
+                'taluks': taluks
+            }
+        except:
+            tid = request.POST['ddlVattam'];
+            villages = TNapi.get_villages(mid.zfill(2), tid.zfill(2))
+            context = {
+                'districts': districts,
+                'taluks': taluks,
+                'villages': villages
+            }
 
+        return render(request, 'adangal/adangal_home.html', context)
+
+        #vid = request.POST['ddlVillage'];
